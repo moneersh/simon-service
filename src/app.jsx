c64433f6a1c96ee098f4f002ad84a11bce777203
MoneerLab <moneer.halabi@byu.edu>
2024-11-17 21:47:59 -0700

    Add this code to service/index.js to allow your code to select a port

--- src/app.jsx
+++ src/app.jsx
@@ -1,48 +1,52 @@
-import React from 'react';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Login } from './login/login';
-import { Play } from './play/play';
-import { Scores } from './scores/scores';
-import { About } from './about/about';
-import { AuthState } from './login/authState';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
+import React from "react";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import { Login } from "./login/login";
+import { Play } from "./play/play";
+import { Scores } from "./scores/scores";
+import { About } from "./about/about";
+import { AuthState } from "./login/authState";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.css";
 
 function App() {
-  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
-  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [userName, setUserName] = React.useState(
+    localStorage.getItem("userName") || ""
+  );
+  const currentAuthState = userName
+    ? AuthState.Authenticated
+    : AuthState.Unauthenticated;
   const [authState, setAuthState] = React.useState(currentAuthState);
 
   return (
     <BrowserRouter>
-      <div className='body bg-dark text-light'>
-        <header className='container-fluid'>
-          <nav className='navbar fixed-top navbar-dark'>
-            <div className='navbar-brand'>
+      <div className="body bg-dark text-light">
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">
               Simon<sup>&reg;</sup>
             </div>
-            <menu className='navbar-nav'>
-              <li className='nav-item'>
-                <NavLink className='nav-link' to=''>
+            <menu className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="">
                   Login
                 </NavLink>
               </li>
               {authState === AuthState.Authenticated && (
-                <li className='nav-item'>
-                  <NavLink className='nav-link' to='play'>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="play">
                     Play
                   </NavLink>
                 </li>
               )}
               {authState === AuthState.Authenticated && (
-                <li className='nav-item'>
-                  <NavLink className='nav-link' to='scores'>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="scores">
                     Scores
                   </NavLink>
                 </li>
               )}
-              <li className='nav-item'>
-                <NavLink className='nav-link' to='about'>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="about">
                   About
                 </NavLink>
               </li>
@@ -52,7 +56,7 @@ function App() {
 
         <Routes>
           <Route
-            path='/'
+            path="/"
             element={
               <Login
                 userName={userName}
@@ -65,17 +69,22 @@ function App() {
             }
             exact
           />
-          <Route path='/play' element={<Play userName={userName} />} />
-          <Route path='/scores' element={<Scores />} />
-          <Route path='/about' element={<About />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/play" element={<Play userName={userName} />} />
+          <Route path="/scores" element={<Scores />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
 
-        <footer className='bg-dark text-dark text-muted'>
-          <div className='container-fluid'>
-            <span className='text-reset'>Author Name(s)</span>
-            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
-              Source
+        <footer className="bg-dark text-dark text-muted">
+          <div className="container-fluid">
+            <span className="text-reset">
+              <i className="fab fa-github"></i> Moneer AL HALABI
+            </span>
+            <a
+              className="text-reset"
+              href="https://github.com/moneersh/simon-react"
+            >
+              <i className="fab fa-github"></i> <span>Source</span>
             </a>
           </div>
         </footer>
@@ -85,7 +94,11 @@ function App() {
 }
 
 function NotFound() {
-  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
+  );
 }
 
 export default App;
